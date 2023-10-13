ffd22c10b3a16b770e4ad0449375388fc1578c30
EsmerlinJM <esmerlinjm@gmail.com>
2023-10-13 17:39:34 -0300

    [REF] Refactor vagrant infra for k8s.

--- infra/vagrant-kubeadm-k8s-cluster/scripts/master.sh
+++ infra/vagrant-kubeadm-k8s-cluster/scripts/master.sh
@@ -36,7 +36,7 @@ kubeadm token create --print-join-command > $config_path/join.sh
 
 # Install Cilium Network Plugin
 
-CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable-v0.14.txt)
 CLI_ARCH=amd64
 if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
 curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
